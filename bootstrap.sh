f064010c09d665180ab18a850e37e29171e0ded0
sawanoboly <sawanoboriyu@higanworks.com>
2015-09-03 19:03:40 +0900

    remove apparmor

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,12 @@
 
 set -ex
 
+# disable apparmor
+sudo /etc/init.d/apparmor stop
+sudo /etc/init.d/apparmor teardown
+sudo update-rc.d -f apparmor remove
+sudo apt-get remove apparmor apparmor-utils -y
+
 sudo apt-get -y update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confnew" -o Dpkg::Options::="--force-confdef" upgrade
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confnew" --force-yes -fuy dist-upgrade
@@ -12,3 +18,4 @@ sudo mkdir -p /etc/chef/ohai/hints/
 sudo touch /etc/chef/ohai/hints/ec2.json
 echo '{}' > ./ec2.json
 sudo mv ./ec2.json /etc/chef/ohai/hints/ec2.json
+exit 0
